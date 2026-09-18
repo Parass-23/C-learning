279c54413b4e97a5713404f8e17df2d51dcf02c9
Paras Nagdevee <parasnag2008@gmail.com>
2026-09-18 23:42:52 +0530

    arrayofstrinf

---
+++ Recursion/preinpost.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+using namespace std;
+void printrec(vector<int>& arr,int idx){
+p
+}
+int main(){
+    vector<int> arr={5,8,2,534,8,235,-8521};
+    printrec(arr,0);
+}
