e290cc414cb6e84a3509e4b5d6f312705dbc41a7
Paras Nagdevee <parasnag2008@gmail.com>
2026-09-20 19:46:22 +0530

    changes

--- OOPS/class_objects.cpp
+++ OOPS/class_objects.cpp
@@ -13,4 +13,14 @@ int main(){
     x.cgpa=9.5;
     x.rno=2;
     cout<<x.name<<" "<<x.cgpa<<" "<<x.rno;
+    Student y;
+    y.name="pramod";
+    y.cgpa=9.0;
+    y.rno=3;
+    cout<<y.name<<" "<<y.cgpa<<" "<<y.rno;
+    Student z;
+    z.name="purva";
+    z.cgpa=8.9;
+    z.rno=4;
+    cout<<z.name<<" "<<z.cgpa<<" "<<z.rno;
 }
